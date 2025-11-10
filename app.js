67b549391a00d4ad0589bc78da1ad93da756639c
nhutvm290 <nhut.vlg2005@gmail.com>
2025-11-10 17:30:37 +0700

    sua doi cac route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
+
 const ApiError = require("./app/api-error");
 
 const app = express();
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactsRouter = require("./app/routes/contact.route");
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
@@ -20,7 +21,8 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   return res
-    .status(error.statusCode || 500)
-    .json({ message: error.message || "Internal Server Error" });
+    .status(err.statusCode || 500)
+    .json({ message: err.message || "Internal Server Error" });
 });
+
 module.exports = app;
