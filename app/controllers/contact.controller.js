c10a86061444364bf5c95adc6f16ff87596f74e9
nhutvm290 <nhut.vlg2005@gmail.com>
2025-11-10 17:01:17 +0700

    dinh nghia cac router cho cac tai nguyen

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req, res) => {
+  res.send({ message: "create handler" });
+};
+
+exports.findAll = (req, res) => {
+  res.send({ message: "findAll hander" });
+};
+
+exports.findOne = (req, res) => {
+  res.send({ message: "findOne hander" });
+};
+
+exports.update = (req, res) => {
+  res.send({ message: "update hander" });
+};
+
+exports.delete = (req, res) => {
+  res.send({ message: "delete hander" });
+};
+
+exports.deleteAll = (req, res) => {
+  res.send({ message: "deleteAll hander" });
+};
+
+exports.findAllFavorite = (req, res) => {
+  res.send({ message: "findAllFavorite hander" });
+};
