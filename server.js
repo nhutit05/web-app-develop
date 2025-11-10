aa233e9e9b6e385f87efaab0d2d44fbff2fdd0f1
nhutvm290 <nhut.vlg2005@gmail.com>
2025-11-10 16:41:45 +0700

    thay doi port

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const app = require("./app")
-const config = require("")
+const app = require("./app");
+const config = require("./app/config");
 
 /**Start Server */
+const PORT = config.app.port;
 
-const PORT = config.app.port
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${PORT}.`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
